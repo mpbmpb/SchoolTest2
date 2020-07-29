a9c6f85b91d465b9dd5a2138dd0b1d3c3c1fb2b1
mpbmpb <markkeyster@gmail.com>
2020-07-29 19:01:53 +0200

    more refactoring on seminarcontroller

--- Controllers/SeminarController.cs
+++ Controllers/SeminarController.cs
@@ -25,11 +25,7 @@ namespace SchoolTest2.Controllers
         // GET: Seminar
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Seminars
-               .Include(s => s.SeminarDays)
-               .ThenInclude(sd => sd.Day)
-               .OrderBy(s => s.Name.ToLower())
-               .ToListAsync());
+            return View(await _db.GetAllSeminarsAsync());
         }
 
         // GET: Seminar/Details/5
@@ -39,63 +35,30 @@ namespace SchoolTest2.Controllers
             {
                 return NotFound();
             }
-            Seminar seminar = await _context.Seminars
-               .Where(s => s.SeminarId == id)
-               .Include(d => d.SeminarDays)
-               .ThenInclude(sd => sd.Day)
-               .ThenInclude(x => x.DaySubjects)
-               .ThenInclude(y => y.Subject)
-               .FirstOrDefaultAsync();
+            Seminar seminar = await _db.GetSeminarIncludingSubjectsAsync((int)id);
             
             if (seminar == null)
             {
                 return NotFound();
             }
-
             return View(seminar);
         }
 
         // GET: Seminar/Create
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
-            var days = _context.Days
-                .Include(d => d.DaySubjects)
-                .ThenInclude(ds => ds.Subject)
-                .ToList();
-            var viewModel = new SeminarViewModel(days);
-
-            return View(viewModel);
+            var days = await _db.GetAllDaysAsync();
+            return View(new SeminarViewModel(days));
         }
 
         // POST: Seminar/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(SeminarViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var seminar = new Seminar();
-                seminar.Name = model.Seminar.Name;
-                seminar.Description = model.Seminar.Description;
-                _context.Add(seminar);
-                await _context.SaveChangesAsync();
-
-                foreach (var item in model.CheckList)
-                {
-                    if (item.IsSelected)
-                    {
-                        SeminarDay sd = new SeminarDay()
-                        {
-                            DayId = item.Id,
-                            SeminarId = seminar.SeminarId
-                        };
-                        _context.Add(sd);
-                    }
-                }
-
-                await _context.SaveChangesAsync();
+                await _db.AddSeminarAsync(model);
                 return RedirectToAction(nameof(Index));
             }
             return View(model);
@@ -109,30 +72,22 @@ namespace SchoolTest2.Controllers
                 return NotFound();
             }
 
-            var seminar = await _context.Seminars
-                .Include(s => s.SeminarDays)
-                .SingleAsync(x => x.SeminarId == id);
+            var seminar = await _db.GetSeminarAsync((int)id);
             if (seminar == null)
             {
                 return NotFound();
             }
-            var days = await _context.Days
-                .Include(d => d.DaySubjects)
-                .ThenInclude(ds => ds.Subject)
-                .ToListAsync();
+            var days = await _db.GetAllDaysAsync();
 
             return View(new SeminarViewModel(seminar, days));
         }
 
         // POST: Seminar/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, SeminarViewModel model)
         {
             int seminarId = model.Seminar.SeminarId;
-            var seminarDays = await _context.SeminarDays.ToListAsync();
             if (id != seminarId)
             {
                 return NotFound();
@@ -140,44 +95,9 @@ namespace SchoolTest2.Controllers
 
             if (ModelState.IsValid)
             {
-                try
-                {
-                    _context.Update(model.Seminar);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!SeminarExists(seminarId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                foreach (var item in model.CheckList)
-                {
-                    var existingEntry = seminarDays.FirstOrDefault(
-                                            x => x.SeminarId == seminarId && x.DayId == item.Id);
-                    if (!item.IsSelected && existingEntry != null)
-                    {
-                        _context.Remove(existingEntry);
-                    }
-
-                    if (item.IsSelected && existingEntry == null)
-                    {
-                        SeminarDay ss = new SeminarDay()
-                        {
-                            SeminarId = seminarId,
-                            DayId = item.Id
-                        };
-                        _context.Add(ss);
-                    }
-                }
-
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                bool success = await _db.UpdateSeminarAsync(model);
+                if (success) { return RedirectToAction(nameof(Index)); }
+                return NotFound();
             }
             return View(model);
         }
--- Data/Db.cs
+++ Data/Db.cs
@@ -48,6 +48,27 @@ namespace SchoolTest2.Data
             await _context.SaveChangesAsync();
         }
 
+        public async Task AddSeminarAsync(SeminarViewModel model)
+        {
+            _context.Add(model.Seminar);
+            await _context.SaveChangesAsync();
+
+            var seminarDays = await _context.SeminarDays.ToListAsync();
+            foreach (var item in model.CheckList)
+            {
+                if (item.IsSelected)
+                {
+                    SeminarDay sd = new SeminarDay()
+                    {
+                        SeminarId = model.Seminar.SeminarId,
+                        DayId = item.Id
+                    };
+                    _context.Add(sd);
+                }
+            }
+            await _context.SaveChangesAsync();
+        }
+
         public async Task<Day> GetDayAsync(int id)
         {
             return await _context.Days.FindAsync(id);
@@ -61,13 +82,40 @@ namespace SchoolTest2.Data
                 .SingleAsync(d => d.DayId == id);
         }
 
-        public async Task<IEnumerable<Day>> GetAllDaysAsync()
+        public async Task<List<Day>> GetAllDaysAsync()
         {
             return await _context.Days
                 .Include(d => d.DaySubjects)
                 .ThenInclude(ds => ds.Subject)
                 .OrderBy(d => d.Name.ToLower())
                 .ToListAsync();
+        }
+
+        public async Task<Seminar> GetSeminarAsync(int id)
+        {
+            return await _context.Seminars
+                .Include(s => s.SeminarDays)
+                .SingleAsync(x => x.SeminarId == id);
+        }
+        
+        public async Task<Seminar> GetSeminarIncludingSubjectsAsync(int id)
+        {
+            return await _context.Seminars
+               .Where(s => s.SeminarId == id)
+               .Include(d => d.SeminarDays)
+               .ThenInclude(sd => sd.Day)
+               .ThenInclude(x => x.DaySubjects)
+               .ThenInclude(y => y.Subject)
+               .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Seminar>> GetAllSeminarsAsync()
+        {
+            return await _context.Seminars
+               .Include(s => s.SeminarDays)
+               .ThenInclude(sd => sd.Day)
+               .OrderBy(s => s.Name.ToLower())
+               .ToListAsync();
         }
 
         public async Task<Subject> GetSubjectAsync(int id)
@@ -143,8 +191,34 @@ namespace SchoolTest2.Data
             return true;
         }
 
-        
+        public async Task<bool> UpdateSeminarAsync(SeminarViewModel model)
+        {
+            bool succes = await UpdateAsync(model.Seminar);
+            if (!succes) { return false; }
+            var seminarDays = await _context.SeminarDays.ToListAsync();
 
+            foreach (var item in model.CheckList)
+            {
+                var existingEntry = seminarDays
+                    .FirstOrDefault(x => x.SeminarId == model.Seminar.SeminarId && x.DayId == item.Id);
+                if (!item.IsSelected && existingEntry != null)
+                {
+                    await RemoveAsync(existingEntry);
+                }
+
+                if (item.IsSelected && existingEntry == null)
+                {
+                    SeminarDay ds = new SeminarDay()
+                    {
+                        SeminarId = model.Seminar.SeminarId,
+                        DayId = item.Id
+                    };
+                    _context.Add(ds);
+                }
+            }
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
     }
-
 }
